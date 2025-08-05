7ea68a02cb8923afcd91ec2f7e05f591050cabfe
matchacookie <tongkhaiminh@gmail.com>
2025-08-05 16:46:55 +0700

    create vue dashboard, create BasicStats sub component

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+  import Dashboard from './components/Dashboard/Dashboard.vue';
+</script>
+
+<template>
+    <!-- <HelloWorld msg="Vite + Vue" /> -->
+     <Dashboard/>
+</template>
+
+<style scoped>
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ frontend/src/components/Dashboard/BasicStats.vue
@@ -0,0 +1,9 @@
+<script>
+    import { ref } from 'vue';
+    const totalHours = ref(1500)
+    const averageBPM = ref (100)
+    // add hardest exercise
+</script>
+<template>
+
+</template>
